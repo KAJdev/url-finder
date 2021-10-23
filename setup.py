d118b28500e1304aa6ecbc15999f054ab2a338ab
kaj <40004347+KAJdev@users.noreply.github.com>
2021-10-23 10:00:07 -0800

    fix issue with trailing '.'

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='url_finder',  
-     version='0.1.3',
+     version='0.1.4',
      author="kajdev",
      description="An easy utility to extract links from text.",
      long_description=long_description,
